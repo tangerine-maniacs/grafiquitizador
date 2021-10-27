da7b33fd511f54fdb5feb7ff1908c1fddab3ac50
Luengor <aluengor0001@gmail.com>
2021-10-27 14:39:36 +0200

    Added graph drawer

--- main.c
+++ main.c
@@ -1,26 +1,60 @@
 #include <stdio.h>
 #include "bmpwriter.h"
 
-#define h 128
-#define w 128
+#define WIDTH 66
+#define HEIGHT 66
+
+// Didn't want to include the math library only for this
+double diff(double a, double b)
+{
+    return (a > b) ? a - b : b - a;
+}
 
 int main()
-{
-  int mat[h][w];
-  for (int i = 0; i < h; i++)
-  {
-    for (int j = 0; j < w; j++)
+{ 
+    // Pixel matrix
+    int mat[HEIGHT][WIDTH] = {};
+    
+    // Graph variables
+    double x_scale = 5, y_scale = 5;
+    double f_tolerance = 3.0e-1;
+    
+    // Function variables
+    double x, y, f;
+
+
+    for (int _y = HEIGHT-1; _y >= 0; _y--)
     {
-      mat[i][j] = (i % 2) && (j % 2);
+        /*
+            current_column-row  = _y - _x       ** Note that _y starts counting from max
+            total_columns-rows  = WIDTH - HEIGHT
+            Offset              = - Scale / 2 
+
+            x = current_column / total_columns * X_Scale - Offset
+            y = current_row / total_rows * Y_Scale - Offset
+        */
+
+        y = (double)_y / (HEIGHT - 1) * y_scale - (y_scale/2.0);
+
+        for (int _x = 0; _x < WIDTH; _x++)
+        {
+            x = (double)_x / (WIDTH - 1) * x_scale - (x_scale/2.0);
+            
+            // Function: y = - x² + 2
+            f = - x * x + 2;
+
+            if (diff(f, y) < f_tolerance)
+            {
+                mat[_y][_x] = 1;
+            }
+            else
+            {
+                mat[_y][_x] = 0;
+            }
+        }
     }
-  }
 
-  // int a = 0x4241;
+    write_bmp("fn.bmp", HEIGHT, WIDTH, mat);
 
-  // printf("\x41\x42");
-  // printf("%c%c", ((unsigned char *)&a)[0], ((unsigned char *)&a)[1]);
-  // print_bytes(&a, sizeof(a));
-  write_bmp("fn.bmp", h, w, mat);
-
-  return 0;
+    return 0;
 }
