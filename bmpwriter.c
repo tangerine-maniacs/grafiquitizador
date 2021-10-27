bb2fd8c33394e09a1d6136b2edfb4b3a31cdd2c9
Pablo Pérez Rodríguez <pablo.rabanales@gmail.com>
2021-10-28 01:23:17 +0200

    Some more debugging statements, plus unsigned byte writing

--- bmpwriter.c
+++ bmpwriter.c
@@ -12,7 +12,15 @@ void print_int(FILE *fp, int a)
 {
   print_bytes(fp, &a, sizeof(a));
 }
+void print_uint(FILE *fp, unsigned int a)
+{
+  print_bytes(fp, &a, sizeof(a));
+}
 void print_short(FILE *fp, short a)
+{
+  print_bytes(fp, &a, sizeof(a));
+}
+void print_ushort(FILE *fp, unsigned short a)
 {
   print_bytes(fp, &a, sizeof(a));
 }
@@ -42,24 +50,25 @@ int write_bmp(char *file_name, size_t rows, size_t columns, int mat[rows][column
   }
 
   //http://www.ece.ualberta.ca/~elliott/ee552/studentAppNotes/2003_w/misc/bmp_file_format/bmp_file_format.htm
+  //https://medium.com/sysf/bits-to-bitmaps-a-simple-walkthrough-of-bmp-image-format-765dc6857393
   // Header (size 14)
-  fprintf(fp, "BM");                    // signature
-  print_int(fp, 0x3e + rows * columns); // bmp size
-  print_int(fp, 0);                     // unused
-  print_int(fp, 0x3e);                  // bitmap image data offset
+  fprintf(fp, "BM");                     // signature
+  print_uint(fp, 0x3e + rows * columns); // bmp size
+  print_uint(fp, 0);                     // unused
+  print_uint(fp, 0x3e);                  // bitmap image data offset
 
   // InfoHeader (size 40)
-  print_int(fp, 40);      // infoheader size
+  print_uint(fp, 40);     // infoheader size
   print_int(fp, columns); // bitmap width
   print_int(fp, rows);    // bitmap height
-  print_short(fp, 1);     // number of planes (don't know what it means)
-  print_short(fp, 1);     // bits per pixel (1 = monochrome)
-  print_int(fp, 0);       // compression (0 = no compression)
-  print_int(fp, 0);       // size of image (0 because compression = 0)
+  print_ushort(fp, 1);    // number of planes (don't know what it means)
+  print_ushort(fp, 1);    // bits per pixel (1 = monochrome)
+  print_uint(fp, 0);      // compression (0 = no compression)
+  print_uint(fp, 0);      // size of image (0 because compression = 0)
   print_int(fp, 0);       // horizontal resolution (px/m)
   print_int(fp, 0);       // vertical resolution (px/m)
-  print_int(fp, 2);       // colours used
-  print_int(fp, 0);       // important colours (0 = all)
+  print_uint(fp, 2);      // colours used
+  print_uint(fp, 0);      // important colours (0 = all)
 
   // ColourTable (size 4*numcolours = 8 bytes)
   // colour 0 (black)
@@ -73,8 +82,8 @@ int write_bmp(char *file_name, size_t rows, size_t columns, int mat[rows][column
   print_byte(fp, 0xff); // blue intensity
   print_byte(fp, 0);    // unused
 
-  // in order to write bytes, i first need to join them
-  // together in 8 bit sequences, and then I can print them.
+  // in order to write bits, i first need to join them
+  // together in 1-byte sequences, and then I can print them.
   // if i'm done reading bytes from the matrix but there
   // are still some bits in the char, i append however many zeroes
   // it's necessary and send that.
@@ -114,10 +123,22 @@ int write_bmp(char *file_name, size_t rows, size_t columns, int mat[rows][column
   // i % 8 tells us how many of those there are.
   // So 8 - (i % 8) tells us how many times we have
   // to shift them left.
-  if (i % 8 != 0)
+  if ((i % 8) != 0)
   {
-    printf("Flushing buffer after loop\n");
+    // Debug statement
+    char bufbinary[11] = "0b";
+    bitchar(bufbinary, buf);
+
+    printf("Flushing buffer after loop. current=%2x (%s)\n", buf, bufbinary);
+    // I don't know whether this should be executed or not.
+    // given a buf=0b00000111,
+    // I don't know whether that should be
+    // 0b0111000, 0b11100000, or 0b00000111, (or maybe even 0b00001110)
     buf <<= 8 - (i % 8);
+
+    bufbinary[2] = '\0';
+    bitchar(bufbinary, buf);
+    printf("Flushed buffer, %2x (%s)\n", buf, bufbinary);
     fprintf(fp, "%c", buf);
   }
 
